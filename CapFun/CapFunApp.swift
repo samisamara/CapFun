8392636ac3c96517f57e9893319248adb80debe9
samisamara <samisamara99@yahoo.com>
2024-03-08 20:04:17 -0600

    Initial Commit

---
+++ CapFun/CapFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapFunApp.swift
+//  CapFun
+//
+//  Created by Sami Samara on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CapFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
